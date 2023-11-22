482ead110b53e0dcda244e36d705c610c767dcd6
Sam Levine <samlevine03@gmail.com>
2023-11-21 23:01:33 -0500

    white

--- src/components/ResumeBuilder/ResumePDF.js
+++ src/components/ResumeBuilder/ResumePDF.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
-// Create styles
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
-    backgroundColor: '#E4E4E4'
+    backgroundColor: 'white'
   },
   section: {
     margin: 10,
@@ -14,7 +13,6 @@ const styles = StyleSheet.create({
   }
 });
 
-// Create Document Component
 const ResumePDF = ({ formData }) => (
   <Document>
     <Page size="A4" style={styles.page}>
